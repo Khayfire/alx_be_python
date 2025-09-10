05eb018c652f7609fe61443d381a1848f3627acb
khayfire <kayode354@gmail.com>
2025-09-10 16:10:02 +0100

    area of a rectangle

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,6 @@
+length = 10
+width = 5
+
+area = length * width
+
+print(f"the area of a rectangle is {area}")
--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,6 +2,6 @@ principal = 1000
 rate = 0.05
 time = 3
 
-simple_interest = 1000 * 0.05 * 3
+interest = 1000 * 0.05 * 3
 
 print(f"the simple interest is {simple_interest}")

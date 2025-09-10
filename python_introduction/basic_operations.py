9bdaa0e8b17fc83d37464f694dfa8d50378334fb
khayfire <kayode354@gmail.com>
2025-09-10 15:55:23 +0100

    python addition subtraction and multiplication

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+sum_result = number1 + number2
+diffrence_result = number2 - number1
+product_result = number1 * number2
+
+
+print(f"addition of {number1} and {number2} is {sum_result}")
+print(f"diffrence of {number1} and {number2} is {diffrence_result}")
+print(f"product of {number1} and {number2} is {product_result}")
